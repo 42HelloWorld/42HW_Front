443dc4e20abc7511b5af32d193fa874fd1c99c54
luckylooky2 <luckylooky2@naver.com>
2023-10-05 15:07:22 +0900

    Test

--- src/components/Call/VoteStatusBoard.tsx
+++ src/components/Call/VoteStatusBoard.tsx
@@ -16,11 +16,7 @@ interface Props {
 const VoteStatusBoard: FC<Props> = ({ totalNum }) => {
   const { socket } = useContext(SocketContext);
   const [voteStatus, setVoteStatus] = useState<string[]>(
-    totalNum === null
-      ? []
-      : [VOTE_SELECT.YES].concat(
-          new Array(totalNum! - 1).fill(VOTE_SELECT.ONGOING)
-        )
+    [VOTE_SELECT.YES].concat(new Array(totalNum! - 1).fill(VOTE_SELECT.ONGOING))
   );
   const indexRef = useRef<number>(1);
   const totalNumRef = useRef<number>(totalNum);
@@ -54,24 +50,20 @@ const VoteStatusBoard: FC<Props> = ({ totalNum }) => {
     <div
       className={`grid grid-cols-${totalNumRef.current} w-full my-1 mx-auto`}
     >
-      {totalNum === null ? (
-        <div>통화가 종료되었습니다.</div>
-      ) : (
-        voteStatus.map((v, i) => (
-          <div key={`voteBlock-${v}-${i}`} className="p-[2px]">
-            <div
-              className={`h-[20px] bg-${
-                v === VOTE_SELECT.ONGOING
-                  ? "gray"
-                  : v === VOTE_SELECT.YES
-                  ? "green"
-                  : "red"
-              }-500`}
-              key={`voteToast + ${v} + ${i}`}
-            />
-          </div>
-        ))
-      )}
+      {voteStatus.map((v, i) => (
+        <div key={`voteBlock-${v}-${i}`} className="p-[2px]">
+          <div
+            className={`h-[20px] bg-${
+              v === VOTE_SELECT.ONGOING
+                ? "gray"
+                : v === VOTE_SELECT.YES
+                ? "green"
+                : "red"
+            }-500`}
+            key={`voteToast + ${v} + ${i}`}
+          />
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Call/VoteToast.tsx
+++ src/components/Call/VoteToast.tsx
@@ -32,33 +32,35 @@ const VoteToast: FC<Props> = ({ contentsName, requester }) => {
   return (
     <div className="flex flex-col justify-evenly">
       <div className="my-1">{`[${requester}] TOPIC-${contentsName} 요청`}</div>
-      <VoteStatusBoard totalNum={callInfo.currNum} />
-      <div className="flex justify-evenly my-1">
-        {callInfo.roomName ? (
-          isVoted === null ? (
-            <>
-              <button
-                className="w-[40%] h-[30px] rounded-md bg-blue-600"
-                onClick={voteAccept}
-              >
-                찬성
-              </button>
-              <button
-                className="w-[40%] h-[30px] rounded-md bg-gray-400"
-                onClick={voteReject}
-              >
-                반대
-              </button>
-            </>
-          ) : isVoted ? (
-            <div>찬성하셨습니다.</div>
-          ) : (
-            <div>반대하셨습니다.</div>
-          )
-        ) : (
-          <div>통화가 종료되었습니다.</div>
-        )}
-      </div>
+      {callInfo.currNum ? (
+        <>
+          <VoteStatusBoard totalNum={callInfo.currNum} />
+          <div className="flex justify-evenly my-1">
+            {isVoted === null ? (
+              <>
+                <button
+                  className="w-[40%] h-[30px] rounded-md bg-blue-600"
+                  onClick={voteAccept}
+                >
+                  찬성
+                </button>
+                <button
+                  className="w-[40%] h-[30px] rounded-md bg-gray-400"
+                  onClick={voteReject}
+                >
+                  반대
+                </button>
+              </>
+            ) : isVoted ? (
+              <div>찬성하셨습니다.</div>
+            ) : (
+              <div>반대하셨습니다.</div>
+            )}
+          </div>
+        </>
+      ) : (
+        <div>통화가 종료되었습니다.</div>
+      )}
     </div>
   );
 };
