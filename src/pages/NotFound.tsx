6aa99085fa48b668d004a1de0f1a669c60ddd9e7
아기새 <82822684+liebespaar93@users.noreply.github.com>
2023-10-07 12:58:14 +0900

    Fix: 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,6 +2,8 @@ import BasicButton from "@utils/BasicButton";
 import { FC, useCallback } from "react";
 import { useNavigate } from "react-router";
 import Loading from "@utils/Loading";
+import gif404 from "@/icon/404.gif"
+import "@/styles/NotFound.css"
 
 interface Props {
   isLoading: boolean;
@@ -17,8 +19,27 @@ const NotFound: FC<Props> = ({ isLoading }) => {
     <Loading />
   ) : (
     <div className="w-full h-full">
-      <div>Page Not Found!</div>
-      <BasicButton text="메인으로 돌아가기" onClick={onBackToMain} />
+      <div id="not-found-cont">
+        <div id="found-title">
+          <h2>Ooph!</h2>
+        </div>
+        <div id="contant">
+          <div id="img-cont">
+            <div id="img-404">
+              <img src={gif404} />
+            </div>
+          </div>
+          <div id="info">
+          <h2>404</h2>
+          <p>Page Not Found~</p>
+          </div>
+        </div>
+        <div id="tail">
+          <div id="butten-cont">
+            <BasicButton text="메인으로 돌아가기" onClick={onBackToMain} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
