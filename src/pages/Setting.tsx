61bee0c750c3943b06693281e4694276d4b77ed1
luckylooky2 <luckylooky2@naver.com>
2023-10-07 15:13:09 +0900

    feat: re-render when mic status changed, toast

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -19,6 +19,7 @@ const Setting = () => {
   const { socket } = useContext(SocketContext);
   const { callInfo, dispatch } = useContext(CallContext);
   const streamArray = useRef<MediaStream[]>([]);
+  const prevStatus = useRef<string>("prompt");
 
   useEffect(() => {
     // 잘못된 접근했을 때
@@ -26,7 +27,14 @@ const Setting = () => {
       stopAllStreams();
       navigate("/main");
     }
+  }, []);
+
+  // 크롬에서는 원래 거부되었을 경우 : denied
+  // 사파리에서는 denied -> prompt 로 바뀌기 때문에 2번 출력
+  useEffect(() => {
     getUserMedia();
+    // console.log(micStatus);
+    // console.log(isDone);
   }, [micStatus, socket]);
 
   useEffect(() => {
@@ -39,10 +47,17 @@ const Setting = () => {
     };
   }, []);
 
+  // denied와 allow / not allow는 다른 것
+  // allow / not allow는 prompt 상태
   const pollMicAvailable = async () => {
     const permissionName = "microphone" as PermissionName;
     const result = await navigator.permissions.query({ name: permissionName });
-    setMicStatus(result.state);
+    if (prevStatus.current !== result.state) {
+      if (result.state === "denied")
+        toast.error("마이크 권한을 허용해 주세요!");
+      setMicStatus(result.state);
+    }
+    prevStatus.current = result.state;
   };
 
   const stopAllStreams = useCallback(() => {
@@ -79,11 +94,14 @@ const Setting = () => {
       setIsDone(true);
       streamArray.current = [...streamArray.current, newStream];
     } catch (e) {
-      toast.error("마이크 권한을 허용해 주세요!");
-      dispatch({ type: CallActionType.DEL_ALL });
+      // 크롬에서는 괜찮은데, 사파리에서는 granted라도 prompt 창이 떠야 연결이 됨
+      // granted 가지고만 판단하면 안 됨
+      // 설정을 변경한 후 꼭 새로고침을 해주세요
+      // safari에서 계속해서 마이크가 켜지지 않는 경우에는 창을 껐다 켜주세요 => 사파리에서는 몇 번 하면 세션에 저장되어 프롬프트가 뜨지 않음
+      dispatch({ type: CallActionType.SET_STREAM, payload: null });
       setIsDone(false);
     }
-  }, []);
+  }, [isDone, micStatus]);
 
   const goToMain = useCallback(() => {
     stopAllStreams();
@@ -110,7 +128,7 @@ const Setting = () => {
         <BasicButton
           onClick={goToWaiting}
           text="매칭 시작하기"
-          disabled={micStatus !== MIC_STATUS.GRANTED}
+          disabled={micStatus !== MIC_STATUS.GRANTED || !isDone}
         />
         <BasicButton onClick={goToMain} text="뒤로 가기" />
       </div>
